d4f90aa57d5373a1c76f252bd203c84513b940e8
themats555 <matt.laurenzi@gmail.com>
2023-05-27 13:51:36 +0200

    First Commit

---
+++ WriteInFile.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 23:53:16 2023
+
+@author: mattei
+"""
+
+class WriteInFile:
+    '''Class to write in a file.
+    Attributes
+    ----------
+        text : str
+            the text to search
+        new_text : str
+            the text to insert
+        sound : str
+            the sound that the animal makes
+        row_number : int
+            the row number after the row of the text (default 1)
+    Example:
+        w = WriteInFile('infile', 'OPS ', 'class_1_ops_',row_number = 3, output_file='outfile')
+        w.write_in_file()
+    '''
+    def __init__(self,input_file, text, new_text, row_number=1, output_file='output_file'):
+        self.input_file = input_file
+        self.text = text
+        self.new_text = new_text
+        self.row_number = row_number
+        self.output_file = output_file
+        
+    def write_in_file(self):
+        fla = 0
+        with open(self.input_file, 'r') as f:
+            with open(self.output_file,'w') as o:
+                for i, line in enumerate(f):
+                    if fla == self.row_number :
+                        o.write(self.new_text+'\n')
+                        fla = 0
+                    elif fla > 0:
+                        fla += 1
+
+                    o.write(line)
+                    if (self.text in line):
+                        fla = 1
+                
+        f.close()
+        o.close()
+        
+
